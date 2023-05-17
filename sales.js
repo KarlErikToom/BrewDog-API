e4717582678f65349f0dfd42df2cf7dc4291507a
Karl <toom.karl42@gmail.com>
2023-05-17 15:01:01 +0300

    started adding product info when you click a beer

--- sales.js
+++ sales.js
@@ -1,19 +1,43 @@
 const beersListEl = document.querySelector(".beers__selection");
+const pageButtons = document.querySelectorAll(".page");
+
 let currentPage = 1;
+const beersPerPage = 8;
 
-async function getBeers(){
-    const beers = await fetch(`https://api.punkapi.com/v2/beers?page=${currentPage}&per_page=8`)
-    const beersData = await beers.json()
-    console.log(beersData)
-    beersListEl.innerHTML = beersData.slice(0,8).map((beer) => beerHTML(beer)).join("")
-}
-getBeers()
+async function getBeers(page) {
+  const startIndex = (page - 1) * beersPerPage;
 
-function  beerHTML(beer){
-    return `<div class="beer">
-    <img class="beer__img" onclick="showBeerPage(${beer.id})" src="${beer.image_url}" alt="">
+  const response = await fetch(`https://api.punkapi.com/v2/beers?page=${page}&per_page=${beersPerPage}`);
+  const beersData = await response.json();
+console.log(beersData)
+  beersListEl.innerHTML = beersData.map(beer => `<div class="beer">
+    <img class="beer__img" onclick="showInfo()" src="${beer.image_url}" alt="">
     <h3 class="beer__name">${beer.name}</h3>
     <p class="beer__type">${beer.tagline}</p>
-</div>
-    `
+  </div>`).join("");
+
+  currentPage = page;
+  updatePaginationButtons();
 }
+
+function updatePaginationButtons() {
+  pageButtons.forEach(button => {
+    const pageNumber = parseInt(button.innerText);
+    button.disabled = pageNumber === currentPage;
+  });
+}
+
+pageButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const page = parseInt(button.innerText);
+    getBeers(page);
+  });
+});
+
+getBeers(currentPage);
+
+/*
+SHOW INFO
+*/
+
+//function showInfo()
